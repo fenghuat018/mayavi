eb23035af2f8ff1da9f7d6982a7884112fa9996c
Fenghua Tong <fenghuat018@gmail.com>
2026-03-28 22:23:09 -0400

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
-import os
 import sys
 
-filename = (
-    os.environ.get("mapreduce_map_input_file")
-    or os.environ.get("map_input_file")
-    or "unknown_file"
-)
-
-display_name = filename.split("/")[-1].replace("__", "/")
-
-count = 0
-
-for line in sys.stdin.buffer:
-    count += 1
-
-print(f"{display_name}\t{count}")
+for line in sys.stdin:
+    print("ok\t1")
