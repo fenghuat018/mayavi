427dbdacfb504e1f18408a6bc51d9611c5465c14
Fenghua Tong <fenghuat018@gmail.com>
2026-03-28 22:31:26 -0400

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 import sys
+import os
+
+print("DEBUG: mapper started", file=sys.stderr)
+print("DEBUG: cwd unknown", file=sys.stderr)
+print("DEBUG: argv =", sys.argv, file=sys.stderr)
+print("DEBUG: python executable OK", file=sys.stderr)
 
 for line in sys.stdin:
     print("ok\t1")
