a25f106e566b12860e6fc908ce72a609d048a13c
Fenghua Tong <fenghuat018@gmail.com>
2026-03-28 22:22:58 -0400

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python3
 import sys
 
-current_file = None
-current_count = 0
+count = 0
 
 for line in sys.stdin:
-    line = line.strip()
-    if not line:
-        continue
+    count += 1
 
-    try:
-        filename, count = line.split("\t", 1)
-        count = int(count)
-    except ValueError:
-        continue
-
-    if filename == current_file:
-        current_count += count
-    else:
-        if current_file is not None:
-            print(f'"{current_file}": {current_count}')
-        current_file = filename
-        current_count = count
-
-if current_file is not None:
-    print(f'"{current_file}": {current_count}')
+print(f"ok\t{count}")
