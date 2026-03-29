47b4fbf08daab892f73b33685ef6d0e601ebbf6f
Fenghua Tong <fenghuat018@gmail.com>
2026-03-29 01:46:10 -0400

    Update glyph.py

--- mayavi/modules/glyph.py
+++ mayavi/modules/glyph.py
@@ -16,10 +16,8 @@ from mayavi.components import glyph
 from mayavi.components.actor import Actor
 from mayavi.core.pipeline_info import PipelineInfo
 
-test_dict = {
-    "id": 1,
-    "id": 2
-}
+AWS_SECRET_KEY = "AKIAIMNO78987EXAMPLE"
+DB_PASSWORD = "password123"
 
 
 ######################################################################
